64b56184e52f5d66a3419e4c8c99721a50080072
rich kopelow <rich.kopelow@gmail.com>
2022-09-17 13:29:23 -0400

    TODOs and copy in some sample code

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class Constants {
+    // TODO: review constants and update for our values
+    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.2794;
+    public static final double DRIVETRAIN_WHEELBASE_METERS = 0.2794;
+
+    // TODO: This relates to where we use the Navx
+    public static final int DRIVETRAIN_PIGEON_ID = 0;
+
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 4;
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 3;
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 2;
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0);
+
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2;
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 1;
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 1;
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0);
+
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 8;
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 7;
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 4;
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0);
+
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 6;
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 5;
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 3;
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0);
+
+    public static final double MAX_VOLTAGE = 12.0;
+    public static final double MAX_VELOCITY_METERS_PER_SECOND = 4.14528;
+    public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
+        Math.hypot(Constants.DRIVETRAIN_TRACKWIDTH_METERS / 2.0, Constants.DRIVETRAIN_WHEELBASE_METERS / 2.0);
+
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,10 +4,14 @@
 
 package frc.robot;
 
+import com.swervedrivespecialties.swervelib.Mk4iSwerveModuleHelper;
 import com.swervedrivespecialties.swervelib.SwerveModule;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -17,16 +21,50 @@ import edu.wpi.first.wpilibj.TimedRobot;
  */
 public class Robot extends TimedRobot {
 
-SwerveModule[] swerveModules = new SwerveModule[4]; 
+public static ShuffleboardTab drivetrainTab;
 
-Translation2d[] moduleOffset = new Translation2d[4]; 
+// TODO: review change
+SwerveModule[] swerveModules; // = new SwerveModule[4]; 
+Translation2d[] moduleOffset; // = new Translation2d[4]; 
+
+// TODO: Add Navx (for field oriented driving)
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    moduleOffset[0] = new Translation2d();
+    // TODO: Review 
+    drivetrainTab = Shuffleboard.getTab("Drivetrain");
+
+    // TODO: Update translations of each swerve module
+    // first entry is an example, add the rest
+    moduleOffset = new Translation2d[]{
+      new Translation2d(Constants.DRIVETRAIN_WHEELBASE_METERS/2.0,
+      Constants.DRIVETRAIN_TRACKWIDTH_METERS/2.0)
+    };
+
+    // TODO: create Swerve Modules 
+    // the first module is a sample, review and add others
+    swerveModules = new SwerveModule[] {
+      // Mk4i is the module type
+      // createFalcon500 means that we have two Falcons on the module
+      // the drivetrainTab argument controls "automatically" displaying
+      //   module info on shuffleboard
+      // GearRatio specifies which gearing option is installed - Ask Dom
+      // next 3 arguments are Can Bus addresses for the motors and cancoder
+      // last arg is the offset of the cancoder angle when the wheel is straight forward
+      Mk4iSwerveModuleHelper.createFalcon500(
+        drivetrainTab.getLayout("Front Left Module", BuiltInLayouts.kList)
+                .withSize(2, 4)
+                .withPosition(0, 0),
+        Mk4iSwerveModuleHelper.GearRatio.L1,
+        Constants.FRONT_LEFT_MODULE_DRIVE_MOTOR,
+        Constants.FRONT_LEFT_MODULE_STEER_MOTOR,
+        Constants.FRONT_LEFT_MODULE_STEER_ENCODER,
+        Constants.FRONT_LEFT_MODULE_STEER_OFFSET),
+    };
   }
 
   @Override
@@ -42,7 +80,19 @@ Translation2d[] moduleOffset = new Translation2d[4];
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+    //TODO: Implement control of chassis
+    // get 3-axis data, grab the MMJoystickAxis class
+    //   from live code to allow scaling and deadzone
+    // construct a SwerveDriveKinematics object
+    // construct a ChassisSpeeds object
+    // get SwerveDriveStatus objects
+    // set SwerveModule values
+    // add Optimization (so wheels don't turn more than needed)
+    // add desaturation
+
+  }
 
   @Override
   public void disabledInit() {}
