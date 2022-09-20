612e42d477392eb3f6c1e35a63ec9b2f5bf848e6
rich kopelow <rich.kopelow@gmail.com>
2022-09-19 20:22:37 -0400

    Add standard "FRC Global" constants

---
+++ src/main/java/frc/robot/utility/MMConstants.java
@@ -0,0 +1,15 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.utility;
+
+/**
+ * MM utility constants
+ */
+public class MMConstants {
+    public static final double kMMFalconTicksPerRev = 2048;
+    public static final double kMMNeoTicksPerRev = 42;
+    public static final int kMMTimeoutMs = 30;
+}
+
